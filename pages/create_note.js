22b6b2670b7535614359a3ba1df3f0155fa80d11
Tugay <tasimtugay@gmail.com>
2025-03-12 15:22:55 +0300

    TextInput's and basic stylization of create note page

--- pages/create_note.js
+++ pages/create_note.js
@@ -1,12 +1,32 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from "react-native";
+
 
 export default function CreateNote({ navigation }) {
+
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+    const [time, setTime] = useState("");
+    const [date, setDate] = useState("");
 
     const goBack = () => {
         navigation.navigate('Home');
     }
 
     const saveNote = () => {
+
+        //Save last edit time
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        setTime(`${hours}:${minutes}:${seconds}`);
+
+        const day = now.getDate().toString().padStart(2, '0');
+        const month = (now.getMonth() + 1).toString().padStart(2, '0');
+        const year = now.getFullYear();
+        setDate(`${day}/${month}/${year}`);
+
         navigation.navigate('Home');
     }
 
@@ -16,7 +36,7 @@ export default function CreateNote({ navigation }) {
             {/* Top Navigation */}
             <View style={styles.topNavContainer}>
                 <TouchableOpacity style={styles.createNoteButton} onPress={goBack}>
-                    <Text style={styles.buttonText}>⬅</Text>
+                    <Text style={styles.buttonText}>☚</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.createNoteButton} onPress={saveNote}>
@@ -26,7 +46,36 @@ export default function CreateNote({ navigation }) {
 
             {/* Content */}
             <View style={styles.content}>
-                <Text style={styles.text}>Aaaaaa</Text>
+
+                <TextInput /* Title */
+                    placeholder="Title"
+                    placeholderTextColor='#3a3a3a'
+                    value={title}
+                    maxLength={60}
+                    numberOfLines={1}
+                    onChangeText={setTitle}
+                    style={
+                        styles.titleInput
+                    }>
+
+                </TextInput>
+                <View style={styles.infoBox}>
+                    <Text style={{ color: '#2a2a2a' }}>Last edit: {time + '  ' + date}</Text>
+                    <Text style={{ color: '#2a2a2a' }}>Characters: {text.length} / 60</Text>
+                </View>
+
+
+                <TextInput /* Note text */
+                    placeholder="Start typing..."
+                    placeholderTextColor='#3a3a3a'
+                    value={text}
+                    maxLength={6000}
+                    onChangeText={setText}
+                    style={
+                        styles.textInput
+                    }>
+
+                </TextInput>
             </View>
         </View>
     )
@@ -58,13 +107,43 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: 48,
         padding: 16,
+        gap: 16
 
+    },
+
+    infoBox: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 16,
+        marginBottom: 24,
+
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: 'white',
+        borderRadius: 12
     },
 
     title: {
         color: '#2995D9',
         fontSize: 16,
         fontWeight: 'bold',
+    },
+
+    titleInput: {
+        width: '100%',
+        color: '#eaeaea',
+        fontSize: 24,
+        padding: 0,
+        margin: 0
+    },
+
+    textInput: {
+        width: '100%',
+        color: '#eaeaea',
+        fontSize: 16,
+        padding: 0,
+        margin: 0,
+        left: 0
     },
 
     text: {
